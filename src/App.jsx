4ba0279631e08e003b450b4f2d5a919845d06b11
Ricardo <ricarditti5@gmail.com>
2026-01-28 18:41:13 +0000

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  //background img na primeira parte
+  return (
+    <>
+      <h1>Tradição, estilo e precisão em cada corte.</h1>
+      <h3>Na Barbearia Legacy, combinamos técnica clássica com estilo moderno. Cortes personalizados, barba bem desenhada e atendimento profissional num ambiente pensado para homens que sabem o que querem.</h3>
+    </>
+  )
+}
+
+export default App
