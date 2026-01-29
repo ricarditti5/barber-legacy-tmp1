0167f34a88646a914ce2b8a77967cafaa35e2e27
Ricardo <ricarditti5@gmail.com>
2026-01-29 08:59:58 +0000

    First impressions of lading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './index.css'
+import barberLogo from '../public/barberLogo.svg';
 
 function App() {
   //background img na primeira parte
+  const [load, setLoad] = useState(0);
   return (
     <>
-      <h1>Tradição, estilo e precisão em cada corte.</h1>
+      <h1>Barbearia Legacy</h1>
+      <img 
+      src={barberLogo}
+      className='w-50 h-50'
+      />
+      <h2>Tradição, estilo e precisão em cada corte.</h2>
       <h3>Na Barbearia Legacy, combinamos técnica clássica com estilo moderno. Cortes personalizados, barba bem desenhada e atendimento profissional num ambiente pensado para homens que sabem o que querem.</h3>
     </>
   )
