00e4a9af8bdd76a8467747c9693f47c725c24fcc
Ricardo <ricarditti5@gmail.com>
2026-01-30 16:22:42 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './index.css'
-import barberLogo from '../public/barberLogo.svg'
 import HeroSec from './Components/HeroSec'
 
 function App() {
@@ -8,13 +7,6 @@ function App() {
   const [load, setLoad] = useState(false);
   return (
     <>
-      <h1>Barbearia Legacy</h1>
-      <img 
-      src={barberLogo}
-      className='w-50 h-50'
-      />
-      <h2>Tradição, estilo e precisão em cada corte.</h2>
-      <h3>Na Barbearia Legacy, combinamos técnica clássica com estilo moderno. Cortes personalizados, barba bem desenhada e atendimento profissional num ambiente pensado para homens que sabem o que querem.</h3>
       <HeroSec   />
     </>
   )
--- src/Components/HeroSec.jsx
+++ src/Components/HeroSec.jsx
@@ -1,11 +1,37 @@
 import {useState} from 'react'
 import './index.css'
-
+import barberLogo from '../public/barberLogo.svg'
+/*<h2>Tradição, estilo e precisão em cada corte.</h2>
+      <h3>Na Barbearia Legacy, combinamos técnica clássica com estilo moderno. Cortes personalizados, barba bem desenhada e atendimento profissional num ambiente pensado para homens que sabem o que querem.</h3>
+      CONTEUDO PARA COLOCAR NA DESCRIÇÃO DA LANDING PAGE */
 export default function HeroSec(props){
     props.name = 2;
     return(
         <>
-            <p>Hello {props.name}</p>
+         <img 
+        src={barberLogo}
+        className='w-50 h-50'
+        />
+        <ul>
+            <li>
+                <h1>Barbearia Legacy</h1>
+            </li>
+            <li>
+                Início
+            </li>
+            <li>
+                Serviços
+            </li>
+            <li>
+                Sobre
+            </li>
+            <li>
+                Galeria de Cortes
+            </li>
+            <li>
+                Provas Sociais
+            </li>
+        </ul>
         </>
     );
 }
